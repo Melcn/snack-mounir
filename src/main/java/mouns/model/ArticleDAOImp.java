a7853e8a4e583677ed768ca567e3f033d32b4de0
Melcn <melaniearinosy@outlook.com>
2023-07-18 11:06:41 +0200

    create DAoimp article

---
+++ src/main/java/mouns/model/ArticleDAOImp.java
@@ -0,0 +1,27 @@
+package mouns.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleDAOImp implements IArticleDAO {
+
+	public void addDish(Dish myDish) {
+
+	}
+
+	public void deleteDish(int id) {
+
+	}
+	public void updateDish(Dish myDish) {
+		
+	}
+	
+	public void getDish(int id) {
+		
+	}
+	
+	public List<Dish> getAllDish(){
+		List<Dish> myDishes = new ArrayList<Dish>();
+		return myDishes;
+	}
+}
